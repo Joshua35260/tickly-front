2dd32147ce50dd51aa8e86e1d0b0240a03bd3add
Joshua Dupin <jdupin@topics.fr>
2024-10-15 03:04:13 +0200

    update prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: "http://localhost:3000/api",
-  fileDownloadUrl: "http://localhost:3000",
-  frontUrl: "http://localhost:4200",
+  production: true,
+  apiUrl: "https://tickly.cloud/api",
+  fileDownloadUrl: "https://tickly.cloud/media",
+  frontUrl: "https://tickly.cloud",
 };
