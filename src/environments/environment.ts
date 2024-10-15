036c493aae7937867bc56f44812aef3c828a2349
Joshua Dupin <jdupin@topics.fr>
2024-10-15 03:12:54 +0200

    update prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,13 @@
 export const environment = {
-  production: true,
-  apiUrl: "https://tickly.cloud/api",
-  fileDownloadUrl: "https://tickly.cloud/media",
-  frontUrl: "https://tickly.cloud",
+  production: false,
+  apiUrl: "http://localhost:3000/api",
+  fileDownloadUrl: "http://localhost:3000",
+  frontUrl: "http://localhost:4200",
 };
+
+// export const environment = { // production
+//   production: true,
+//   apiUrl: "https://tickly.cloud/api",
+//   fileDownloadUrl: "https://tickly.cloud/media",
+//   frontUrl: "https://tickly.cloud",
+// };
